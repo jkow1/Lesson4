2c420e36f2000f3c25424bea61967c07918ede16
jkow1 <jkowalczuk1@sii.pl>
2022-08-19 10:32:56 +0200

    Small fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         if (!(new Car()).findAndDisplayCountry((new Factory()).generateCarsObjects(), "BMW", true, 300)) {
-            System.out.println("No coutry founded");
+            System.out.println("Country not found");
         }
 
     }
